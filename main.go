b21b09456923fb76cec34c904ea5919fe5ac21a1
Katy Moe <katy@katy.moe>
2019-10-06 20:25:38 +0100

    update provider scaffolding so it compiles with SDK dependency

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+	"github.com/terraform-providers/terraform-provider-scaffolding/scaffolding" // change this to the import path of your provider
+)
+
 func main() {
-	/*plugin.Serve(&plugin.ServeOpts{
-	ProviderFunc: opc.Provider})*/
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: scaffolding.Provider})
 }
---
+++ scaffolding/provider.go
@@ -0,0 +1,13 @@
+package scaffolding
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		DataSourcesMap: map[string]*schema.Resource{},
+		ResourcesMap:   map[string]*schema.Resource{},
+	}
+}
