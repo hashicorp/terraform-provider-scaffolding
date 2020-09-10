3c646ab7c0ce661309046a74941516e3ca7d3356
Katy Moe <katy@katy.moe>
2020-09-10 13:28:59 +0100

    upgrade to SDK v2.0.1

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/hashicorp/terraform-provider-scaffolding
 
 go 1.12
 
-require github.com/hashicorp/terraform-plugin-sdk v1.4.1
+require github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.1
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,11 +1,10 @@
 package provider
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func New() terraform.ResourceProvider {
+func New() *schema.Provider {
 	return &schema.Provider{
 		DataSourcesMap: map[string]*schema.Resource{
 			"scaffolding_data_source": dataSourceScaffolding(),
