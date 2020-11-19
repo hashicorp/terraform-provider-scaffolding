3c87a86ff2eeb6b0835e26b16f2bf8bc293622fd
Paul Tyng <ptyng@hashicorp.com>
2020-11-19 18:56:37 -0500

    Add debug scaffolding (#34)
    Fixes #29

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/hashicorp/terraform-provider-scaffolding/internal/provider"
 )
@@ -15,5 +19,21 @@ var (
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{ProviderFunc: provider.New(version)})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{ProviderFunc: provider.New(version)}
+
+	if debugMode {
+		// TODO: update this string with the full name of your provider as used in your configs
+		err := plugin.Debug(context.Background(), "registry.terraform.io/hashicorp/scaffolding", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
