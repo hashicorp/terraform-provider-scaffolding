81d4a165402036fa054d586fc0bf04772bf43bad
Brian Flad <bflad417@gmail.com>
2022-03-15 11:50:52 -0400

    main: Use new `ServeOpts` type `Debug` field to simplify main function (#104)
    Reference: https://github.com/hashicorp/terraform-plugin-sdk/pull/857

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/hashicorp/terraform-provider-scaffolding/internal/provider"
@@ -34,15 +32,13 @@ func main() {
 	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
-	opts := &plugin.ServeOpts{ProviderFunc: provider.New(version)}
+	opts := &plugin.ServeOpts{
+		Debug: debugMode,
 
-	if debugMode {
 		// TODO: update this string with the full name of your provider as used in your configs
-		err := plugin.Debug(context.Background(), "registry.terraform.io/hashicorp/scaffolding", opts)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		return
+		ProviderAddr: "registry.terraform.io/hashicorp/scaffolding",
+
+		ProviderFunc: provider.New(version),
 	}
 
 	plugin.Serve(opts)
